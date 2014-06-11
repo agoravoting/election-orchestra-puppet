d4137841f5bdcf1758b3cc014ffe41e3156fa7d4
Eduardo Robles Elvira <edulix@wadobo.com>
2014-06-12 01:48:34 +0200

    fix debian

--- shell/apt.sh
+++ shell/apt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # for up to date version of nginx
-# http://danielmiessler.com/blog/upgrading-to-nginx-1-4-x-on-ubuntu#.UkyqAYYerA0
+# http://nginx.org/en/linux_packages.html#stable
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 
-grep 'http://nginx.org/packages/ubuntu/ precise nginx' /etc/apt/sources.list || echo 'deb http://nginx.org/packages/ubuntu/ precise nginx' >> /etc/apt/sources.list
+grep 'http://nginx.org/packages/debian/ wheezy nginx' /etc/apt/sources.list || echo 'deb http://nginx.org/packages/debian/ wheezy nginx' >> /etc/apt/sources.list
 
 apt-get update
 # http://stackoverflow.com/questions/13018626/add-apt-repository-not-found
@@ -12,7 +12,7 @@ apt-get -y install python-software-properties software-properties-common htop su
 # java
 # http://www.webupd8.org/2014/03/how-to-install-oracle-java-8-in-debian.html
 grep 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' /etc/apt/sources.list || echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-apt-get -y install aptitude realpath
+apt-get -y --force-yes install aptitude realpath
 
 # puppet-python module
 SCRIPT_PATH=$(readlink -f "$0")
@@ -22,10 +22,10 @@ then
     git clone git://github.com/stankevich/puppet-python.git python
 fi
 
-wget -qO /tmp/puppetlabs-release-precise.deb https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+wget -qO /tmp/puppetlabs-release-wheezy.deb https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 
-dpkg -i /tmp/puppetlabs-release-precise.deb
-rm /tmp/puppetlabs-release-precise.deb
+dpkg -i /tmp/puppetlabs-release-wheezy.deb
+rm /tmp/puppetlabs-release-wheezy.deb
 
 echo "saving path to manifests/init.pp so that backup script knows.."
 
@@ -49,11 +49,11 @@ echo Installing java..
 if [ ! -f /usr/bin/java ]
 then
     echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-    while ! apt-get install -y oracle-java8-installer
+    while ! apt-get install -y --force-yes oracle-java8-installer
     do
         echo "Failed to download java, retrying in 10 seconds.."
         sleep 10
     done
-    apt-get install oracle-java8-set-default
+    apt-get install --force-yes oracle-java8-set-default
 fi
 echo Java installed
